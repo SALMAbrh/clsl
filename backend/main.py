ecd69119a397ad448c97296abcd43f6dba762043
salmaberrahou <155955354+SALMAbrh@users.noreply.github.com>
2024-12-06 14:43:49 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,15 @@
+import os
 from app import create_app, db
- 
+
 app = create_app()
- 
+
+# Créer les tables dans SQLite si elles n'existent pas (uniquement au démarrage, avant le déploiement)
+with app.app_context():
+    db.create_all()
+    print("Base de données SQLite initialisée avec succès !")
+
 if __name__ == "__main__":
-    # Initialiser la base de données
-    with app.app_context():
-        db.create_all()  # Crée les tables dans SQLite si elles n'existent pas
-        print("Base de données SQLite initialisée avec succès !")
-    app.run(debug=True)
+    # Récupérer le port depuis l'environnement ou utiliser 5000 par défaut
+    port = int(os.environ.get("PORT", 5000))
+    # Lancer l'application sur le port donné par Azure
+    app.run(host="0.0.0.0", port=port, debug=True)
